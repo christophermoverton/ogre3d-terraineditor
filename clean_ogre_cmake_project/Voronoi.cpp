284e8e8522bafd30037fc6c0e5272c862e91438f
Christopher M Overton <christopermoverton@gmail.com>
2014-12-13 07:53:03 -0600

    update

--- clean_ogre_cmake_project/Voronoi.cpp
+++ clean_ogre_cmake_project/Voronoi.cpp
@@ -102,9 +102,21 @@ vector<double> Voronoi::nearestNodes2(vector<double> pos){
 			mnode21 = node2_t[1]/node2_t[0];
 			mpos_t1 = pos_t[1]/pos_t[0];
 			//under the new coordinate system we say the point dist2n1 
-			//is y = mx or (mx^2 + x^2)^1/2 = 2*distn2n1 means
-			//4*distn2n1^2/(m+1) = x^2 or x = 2 distn2n1 /(m+1)^1/2
-			//and y = 2m/(m+1)^1/2 * distn2n1 		
+			//is y = mx or (m^2x^2 + x^2)^1/2 = 2*distn2n1 means
+			//4*distn2n1^2/(m^2+1) = x^2 or x_0 = 2 distn2n1 /(m^2+1)^1/2
+			//and y_0 = 2m/(m^2+1)^1/2 * distn2n1 
+			// point slope formula is given by (y-y0) = m(x-x0).
+			//Our direction vector at such point is defined by a 90 degree rotation
+			// of the distance vector (between both nodes) or (-y_d,x_d) where
+			// x_d and y_d are the original direction coordinates of such vector.
+			// so where our original m = node2_t[1]/node2_t[0]
+			// then an orthogonal direction vector is m_n = 1/m = -node2_t[0]/node2_t[1].
+			// Then using point slope formula we have
+			// (y-2m/(m^2+1)^1/2*distn2n1) = m_n*(x - 2*distn2n1/(m^2+1)^1/2)
+			// y - m_n*x -2m/(m^2+1)^1/2 * distn2n1 + 2*m_n*distn2n1/(m^2+1)^1/2 = 0
+			//now we make use of distance between point (x_0,y_0) and a line.
+			//distance (ax +by + c) = |ax_0 + by_0 + c|/(a^2 + b^2)^1/2
+			// This is given by abs(2*m/(m+1)^1/2*x_0+y_0)/(4m^2/(m+1)+1)	
 		}	
 		//next we mirror the ndirvec.  We need to perform a mirror test
 		
