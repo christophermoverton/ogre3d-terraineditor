1db8530b077a23e07ab6be2adff00e153aa78cdd
Christopher M Overton <christopermoverton@gmail.com>
2015-01-31 17:34:14 -0600

    update

--- clean_ogre_cmake_project/TPoint3.h
+++ clean_ogre_cmake_project/TPoint3.h
@@ -8,7 +8,8 @@ struct TPoint3
 		double x, y, z;
 	
 		/*
-			Constructor for structure; x, y, z - coordinates
+			Constructor for structure; x, y, z - coordinates.  Like an Ogre::Vector3 class...more limited
+			at the moment though...uses double instead of float for potentially greater numerical accuracy.
 		*/
 
 	    	TPoint3(double nx, double ny, double nz) 
@@ -34,6 +35,11 @@ struct TPoint3
 		bool operator!=(const TPoint3 & a) const{
 			return (x != a.x || y != a.y || z != a.z);
 		}
+
+		TPoint3 operator+(const TPoint3 & a) const
+    		{
+        		return TPoint3(a.x+x, a.y+y, a.z+z);
+    		}
 };
 
 #endif
